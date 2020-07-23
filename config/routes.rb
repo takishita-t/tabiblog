a3f017e1f546862d2c83f62d4072cd8b0e2b3918
Ubuntu <ubuntu@ip-172-31-44-226.ap-northeast-1.compute.internal>
2020-07-23 10:23:14 +0000

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, :controllers =>{
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  
+  resources :articles
+  root "articles#index"
+end
